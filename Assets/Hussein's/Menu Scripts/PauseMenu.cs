8947af26fd4b7ac65691857b9240b13b179598f6
Mamadou <m.hussein@saedubai.com>
2022-11-05 03:42:23 +0400

    Pause menu functionality

---
+++ Assets/Hussein's/Menu Scripts/PauseMenu.cs
@@ -0,0 +1,78 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+
+    public GameObject pauseMenu;
+    // public GameObject HUD;
+    // public GameObject Cam;
+
+    public bool isPaused;
+    void Start()
+    {
+        pauseMenu.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (!isPaused)
+            {
+                PauseGame();
+                Cursor.lockState = CursorLockMode.Confined;
+                //HUD.SetActive(false);
+                //Cam.GetComponent<CameraController>().enabled = false;
+            }
+            else if (isPaused)
+            {
+                ResumeGame();
+                Cursor.lockState = CursorLockMode.Locked;
+
+
+
+
+            }
+        }
+    }
+
+    public void PauseGame()
+    {
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0f;
+        isPaused = true;
+    }
+
+    public void ResumeGame()
+    {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+        isPaused = false;
+        Cursor.lockState = CursorLockMode.Locked;
+        // HUD.SetActive(true);
+        // Cam.GetComponent<CameraController>().enabled = true;
+    }
+
+    public void RestartLevel()
+    {
+        string levelName = SceneManager.GetActiveScene().name;
+        SceneManager.LoadScene(levelName);
+        ResumeGame();
+    }
+
+    public void GoToMainMenu()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("HusseinScene");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+}
