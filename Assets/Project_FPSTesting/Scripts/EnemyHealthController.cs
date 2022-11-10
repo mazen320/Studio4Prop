b34b4313699bc9b3045b80c859155b619c207eb4
mazen320 <mazenshaar@hotmail.com>
2022-11-10 04:52:57 +0400

    Added win screen & damage/health for prop

--- Assets/Project_FPSTesting/Scripts/EnemyHealthController.cs
+++ Assets/Project_FPSTesting/Scripts/EnemyHealthController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class EnemyHealthController : MonoBehaviour
 {
@@ -24,6 +25,8 @@ public class EnemyHealthController : MonoBehaviour
         if(currentHealth <= 0)
         {
             Destroy(gameObject);
+            SceneManager.LoadScene(2);
+            //Cursor.lockState = CursorLockMode.None;
         }
     }
 }
