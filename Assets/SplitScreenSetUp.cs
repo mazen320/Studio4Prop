2a777f404f217fea7a9644d5d97da1c5f7ce0c10
Mamadou <m.hussein@saedubai.com>
2022-11-10 04:28:45 +0400

    Added cheating cover for hunter turn (NO CHEATING)

--- Assets/SplitScreenSetUp.cs
+++ Assets/SplitScreenSetUp.cs
@@ -7,24 +7,25 @@ public class SplitScreenSetUp : MonoBehaviour
     
     public GameObject prop;
     public GameObject hunter;
+    public GameObject cheatingScreen;
     void Start()
     {
-        
     }
-
-    
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Backspace)) 
         {
            prop.GetComponent<Movement>().enabled = false;
-        hunter.GetComponent<PlayerMovement>().enabled = true;
+        hunter.GetComponent<PlayerController>().enabled = true;
+            cheatingScreen.SetActive(true);
         }
+      
 
         if (Input.GetKeyDown(KeyCode.Equals))
         {
             prop.GetComponent<Movement>().enabled = true;
-            hunter.GetComponent<PlayerMovement>().enabled = false;
+            hunter.GetComponent<PlayerController>().enabled = false;
+            cheatingScreen.SetActive(false);
         }
     }
 
