43bc5f87fb77fa2a2e5497ad023d280fde3eac45
myoussef57 <misho.257@hotmail.com>
2022-11-17 15:09:55 +0400

    clean - maz

--- Assets/Scripts/SplitScreenSetUp.cs
+++ Assets/Scripts/SplitScreenSetUp.cs
@@ -4,36 +4,53 @@ using UnityEngine;
 
 public class SplitScreenSetUp : MonoBehaviour
 {
-    
+
     public GameObject prop;
     public GameObject hunter;
     public GameObject cheatingScreen;
     public GameObject propCamera;
 
     public Rigidbody propRB;
-    
+
+    bool playingProp = false;
+    bool playingHunter = false;
+
     void Start()
     {
+        playingProp = true;
     }
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Backspace)) 
+        if (playingProp)
         {
-        prop.GetComponent<Movement>().enabled = false;
-        hunter.GetComponent<PlayerController>().enabled = true;
-        cheatingScreen.SetActive(true);
-        propRB.isKinematic = true;
-            propCamera.GetComponent<PropCameraControll>().enabled = false;
-        }
-      
 
-        if (Input.GetKeyDown(KeyCode.Equals))
-        {
             prop.GetComponent<Movement>().enabled = true;
             hunter.GetComponent<PlayerController>().enabled = false;
             cheatingScreen.SetActive(false);
             propRB.isKinematic = false;
             propCamera.GetComponent<PropCameraControll>().enabled = true;
+            playingHunter = false;
+
+        }
+        {
+            if (Input.GetKeyDown(KeyCode.Backspace))
+            {
+                playingProp = false;
+                prop.GetComponent<Movement>().enabled = false;
+                hunter.GetComponent<PlayerController>().enabled = true;
+                cheatingScreen.SetActive(true);
+                propRB.isKinematic = true;
+                propCamera.GetComponent<PropCameraControll>().enabled = false;
+                playingHunter = true;
+            }
+
+            else
+            {
+                if (Input.GetKeyDown(KeyCode.Equals))
+                {
+                    playingProp = true;
+                }
+            }
         }
     }
 
