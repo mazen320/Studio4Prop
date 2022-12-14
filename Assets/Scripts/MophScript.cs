79719c93d92249147fabc608ec108847596e1c8c
myoussef57 <misho.257@hotmail.com>
2022-12-14 14:26:04 +0400

    Morphing fixes

--- Assets/Scripts/MophScript.cs
+++ Assets/Scripts/MophScript.cs
@@ -10,27 +10,29 @@ public class MophScript : MonoBehaviour
     MeshRenderer renderer;
     MeshFilter filter;
     MeshCollider collider;
-    //Transform transformcomp;
+    public Transform modelTransform;
 
 
     void Start()
     {
         Cursor.lockState = CursorLockMode.Locked;
         cam = Camera.main;
-        renderer = GetComponent<MeshRenderer>();
-        filter = GetComponent<MeshFilter>();
-        collider = GetComponent<MeshCollider>();
+        renderer = GetComponentInChildren<MeshRenderer>();
+        filter = GetComponentInChildren<MeshFilter>();
+        
     }
 
     void Update()
     {
         if (Input.GetKeyDown(morph))
         {
+            Debug.Log("husssein");
             RaycastHit hit;
             //checking what is hit withing certain layermask & range
             if (Physics.Raycast(transform.position, transform.forward, out hit, detectRange, mask))
             {
                 //copy and replace the filter
+                Debug.Log(hit.collider.gameObject.name);
                 MeshFilter hitFilt = hit.collider.gameObject.GetComponent<MeshFilter>();
                 filter.mesh = hitFilt.mesh;
 
@@ -39,11 +41,11 @@ public class MophScript : MonoBehaviour
                 renderer.material = hitRend.material;
 
                 //copy and replace the collider
-                MeshCollider hitCollider = hit.collider.gameObject.GetComponent<MeshCollider>();
-                collider.sharedMesh = hitCollider.sharedMesh;
+                //MeshCollider hitCollider = hit.collider.gameObject.GetComponent<MeshCollider>();
+                //collider.sharedMesh = hitCollider.sharedMesh;
 
-                //Transform trans = hit.transform.gameObject.GetComponent<Transform>();
-                //transform.localScale = trans.localScale;
+                Transform trans = hit.transform.gameObject.GetComponent<Transform>();
+                modelTransform.localScale = trans.localScale;
             }
         }
     }
