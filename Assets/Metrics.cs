0a3ec80b7036d3bb712e82cd6c474d5affe9cf69
myoussef57 <misho.257@hotmail.com>
2022-12-20 15:52:56 +0300

    Metrics system now tracks play time.

--- Assets/Metrics.cs
+++ Assets/Metrics.cs
@@ -5,27 +5,43 @@ using System.IO;
 
 public class Metrics : MonoBehaviour
 {
-    void CreateText()
+    public static float secondsCount;
+    public static int minuteCount;
+    private static int hourCount;
+    public static void CreateText()
     {
         string path = Application.dataPath + "/Log.txt";
         if (!File.Exists(path))
         {
             File.WriteAllText(path, "Player time Log \n\n");
         }
-        string content = "Login date:" + System.DateTime.Now + "\n";
+        string content = "Login date:" + System.DateTime.Now + "\n" + "Player time" + secondsCount + "secs" + minuteCount + "mins" + hourCount + "hours" + "\n";
         File.AppendAllText(path, content);
     }
 
-    
+    public void OnApplicationQuit()
+    {
+        CreateText();
+    }
     // Start is called before the first frame update
     void Start()
     {
-        CreateText();
+       
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        secondsCount += Time.deltaTime;
+        if (secondsCount >= 60)
+        {
+            minuteCount++;
+            secondsCount = 0;
+        }
+        else if (minuteCount >= 60)
+        {
+            hourCount++;
+            minuteCount = 0;
+        }
     }
 }
