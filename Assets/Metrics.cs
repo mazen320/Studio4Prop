3b8ee3f05b6baa29429eaa5849ca365f7957976d
myoussef57 <misho.257@hotmail.com>
2022-12-25 16:25:15 +0300

    Added Post-processing and some polish changes
    -added controls text in UI
    -added Bloom, Color grading, and Depth of field Post-processing effects
    -changed the tutorial button in the pause menu to the controls button

--- Assets/Metrics.cs
+++ Assets/Metrics.cs
@@ -23,12 +23,7 @@ public class Metrics : MonoBehaviour
     {
         CreateText();
     }
-    // Start is called before the first frame update
-    void Start()
-    {
-       
-    }
-
+   
     // Update is called once per frame
     void Update()
     {
