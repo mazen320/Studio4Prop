b62d6710d716aef2201d296676c2394aa3480f3e
Mamadou <m.hussein@saedubai.com>
2022-12-26 16:24:19 +0400

    Added battle pass.

--- Assets/Hussein's/ShopManager.cs
+++ Assets/Hussein's/ShopManager.cs
@@ -8,20 +8,17 @@ public class ShopManager : MonoBehaviour
 {
     [SerializeField] private int _coins;
     [SerializeField] private TMP_Text _coinUI;
-    //public ShopItemSO[] shopItemsSO;
-    //public GameObject[] shopPanelsGO;
+ 
     public ShopTemplate[] shopPanels;
-    //public Button[] myPurchaseBtns;
+   
     public static int Coins { get; private set; } = 0;
 
 
     private void Start()
     {
-        //for (int i = 0; i < shopItemsSO.Length; i++)
-        //    shopPanelsGO[i].gameObject.SetActive(true);
+  
         _coinUI.text = "Coins: " + _coins.ToString();
-        ////LoadPanels();
-        ////CheckPurchasable();
+     
     }
 
     private void Update()
@@ -34,42 +31,10 @@ public class ShopManager : MonoBehaviour
         Coins += coins;
         _coins = Coins;
         _coinUI.text = $"Coins: {Coins}";
-        //CheckPurchasable();
+        
     }
 
 
-    //public void CheckPurchasable()
-    //{
-    //    for(int i = 0; i < shopItemsSO.Length; i++)
-    //    {
-    //        if (coins >= shopItemsSO[i].baseCost)
-    //            myPurchaseBtns[i].interactable = true;
-    //        else
-    //            myPurchaseBtns[i].interactable=false;
-    //    }
-    //}
-
-//public void PurchaseItem(int btnNo)
-//{
-//    if (coins >= shopItemsSO[btnNo].baseCost)
-//        {
-//            coins = coins - shopItemsSO[btnNo].baseCost;
-//            coinUI.text = "Coins: " + coins.ToString();
-//            CheckPurchasable();
-
-//        }
-//}
-
-    //public void LoadPanels()
-    //{
-    //    for (int i = 0; i < shopItemsSO.Length; i++)
-    //    {
-    //        shopPanelsGO[i].titleTxt.text = shopItemsSO[i].title;
-    //        shopPanelsGO[i].descriptionTxt.text = shopItemsSO[i].description;
-    //        shopPanelsGO[i].costTxt.text = "Coins: " + shopItemsSO[i].baseCost.ToString();
-    //    }
-        
-    //}
 
 }
 
